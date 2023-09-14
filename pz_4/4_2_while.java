8dd397b38c0060ad3d3e906a5a5bf46d0db65417
cherrra <94289157+cherrra@users.noreply.github.com>
2023-09-14 22:46:48 +0300

    commit

--- pz_4/4_2_while.java
+++ pz_4/4_2_while.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 2 числа: ");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int sum = 0;
-        for (int i = a; i <= b; i++) {
-            sum += i;
+        while (a <= b) {
+            sum += a;
+            a++;
         }
-        System.out.println("Сумма чисел от " + a + " до " + b + ": " + sum);
+        System.out.println(sum);
     }
 }
