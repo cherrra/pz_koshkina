c46f13973e4096cf38affbbc01e738019d368afe
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-19 15:18:47 +0300

    commit

---
+++ pz_12/Main.java
@@ -0,0 +1,80 @@
+/* //методы
+public class Main {
+    public static void main(String[] args) {
+        Animal tiger = new Animal();
+        tiger.type = "тигр";
+        tiger.name = "Артём";
+        tiger.age = 15;
+        tiger.weight = 300.6;
+        tiger.isSwim = true;
+        tiger.isWalk = true;
+        tiger.isFly = false;
+        tiger.display();
+        tiger.rename("Антон");
+        tiger.display();
+
+
+        Animal sparrow = new Animal();
+        sparrow.type = "воробей";
+        sparrow.name = "Капитан Джек";
+        sparrow.age = 3;
+        sparrow.weight = 2;
+        sparrow.isSwim = false;
+        sparrow.isWalk = true;
+        sparrow.isFly = true;
+        sparrow.display();
+        sparrow.holiday(2);
+        sparrow.display();
+    }
+} */
+
+/* //конструкторы
+public class Main {
+    public static void main(String[] args) {
+        Animal tiger = new Animal("тигр", "Артём");
+        tiger.age = 15;
+        tiger.weight = 300.6;
+        tiger.isSwim = true;
+        tiger.isWalk = true;
+        tiger.isFly = false;
+        tiger.display();
+
+        Animal sparrow = new Animal("воробей", 5);
+        sparrow.weight = 2;
+        sparrow.isSwim = false;
+        sparrow.isWalk = true;
+        sparrow.isFly = true;
+        sparrow.display();
+
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.holiday(5);
+        duck.rename("Утя-Утя");
+        duck.display();
+    }
+} */
+
+//инкапсуляция
+
+public class Main {
+    public static void main(String[] args) {
+        Animal tiger = new Animal("тигр", "Артём");
+        tiger.setAge(15);
+        tiger.setWeight(300.6);
+        tiger.setSwim(true);
+        tiger.setWalk(true);
+        tiger.setFly(false);
+        tiger.display();
+
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.holiday(5);
+        duck.rename("Утя-Утя");
+        duck.display();
+        System.out.println(duck.getAge());
+        System.out.println(duck.getName());
+        System.out.println(duck.getWeight());
+        System.out.println(duck.getType());
+        System.out.println(duck.getSwim());
+        System.out.println(duck.getWalk());
+        System.out.println(duck.getFly());
+    }
+}
