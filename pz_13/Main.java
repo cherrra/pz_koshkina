8b30755d6be3cd0ec20dd59b3b66457e3a5e4322
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-21 20:12:04 +0300

    commit

---
+++ pz_13/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.display();
+
+        Bird b = new Bird();
+        b.setName("Bob");
+        b.display();
+        b.setArea("На Югах");
+        b.setWinterFly(false);
+        System.out.println(b.getArea());
+        System.out.println(b.isWinterFly());
+        b.chiriсk_chiriсk();
+
+        Fish f = new Fish();
+        f.setName("Сельд");
+        f.display();
+        f.setSquama("Крупная");
+        f.setUpStreamSwim(false);
+        System.out.println(f.getSquama());
+        System.out.println(f.isUpStreamSwim());
+        f.bul_bul();
+
+        Insect i = new Insect();
+        i.setName("Жук Жукыч");
+        i.display();
+        i.setWingCount(4);
+        i.setLikeJesus(true);
+        System.out.println(i.getWingCount());
+        System.out.println(i.isLikeJesus());
+        i.ggggg();
+
+     /* // перегрузка
+        duck.holiday();
+        duck.display();
+
+        duck.holiday(5);
+        duck.display();
+
+        duck.holiday(0.4);
+        duck.display();
+
+        duck.holiday(0.3, 2);
+        duck.display(); */
+    }
+}
