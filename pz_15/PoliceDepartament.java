9adb6a83479b72065436336e556586a51bf7dcbe
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-23 14:15:35 +0300

    commit

---
+++ pz_15/PoliceDepartament.java
@@ -0,0 +1,71 @@
+public class PoliceDepartament extends Building implements SocialHouse {
+    int count_sotryd;
+    int year_repair;
+    String name_city;
+
+    PoliceDepartament(String name, String adres, int yearBuilt, String architectName, boolean isCultural, int count_sotryd, int year_repair, String name_city) {
+        this.name = name; this.adres = adres; this.yearBuilt = yearBuilt; this.architectName = architectName; this.isCultural = isCultural;
+        this.count_sotryd = count_sotryd;
+        this.year_repair = year_repair;
+        this.name_city = name_city;
+    }
+
+    //инкапсуляция
+
+    int getCount_books() {
+        return this.count_sotryd;
+    }
+
+    void setCount_books(int count_sotryd) {
+        this.count_sotryd = count_sotryd;
+    }
+
+    int getYear_repair() {
+        return this.year_repair;
+    }
+
+    void setYear_repair(int year_repair) {
+        this.year_repair = year_repair;
+    }
+
+    String getName_city() {
+        return this.name_city;
+    }
+
+    void setName_city(String name_city) {
+        this.name_city = name_city;
+    }
+
+    @Override
+    public String toString() {
+        return this.count_sotryd + " " + this.year_repair + " " + this.name_city;
+    }
+
+
+    void display() {
+        System.out.println("Полицейский участок");
+    }
+
+    void showArch() {
+        System.out.println("Здание спроектировал " + architectName);
+    }
+
+    void showtoRepair() {
+        System.out.println("Ремонт в здании был проведён в " + this.year_repair);
+    }
+
+
+    public void information() {
+        System.out.println("Это полицейский участок");
+    }
+
+    public void count_any() {
+        System.out.println("Кол-во сотрудников: " + this.count_sotryd);
+    }
+
+    public void year_house() {
+        System.out.println("Здание построено в: " + this.yearBuilt);
+    }
+
+
+}
