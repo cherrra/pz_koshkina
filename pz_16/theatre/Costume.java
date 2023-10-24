654e2635ea87962d155335f0e8329388b4526797
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-24 23:29:40 +0300

    commit

---
+++ pz_16/theatre/Costume.java
@@ -0,0 +1,25 @@
+public class Costume {
+    private String name;
+    private String description;
+
+    public Costume(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
