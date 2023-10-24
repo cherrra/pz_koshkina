032d44bbf17b2864745359d36d336ee4a831e818
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-24 23:30:18 +0300

    commit

---
+++ pz_16/theatre/Ticket.java
@@ -0,0 +1,35 @@
+public class Ticket {
+    private int number;
+    private int price;
+    private String date;
+
+    public Ticket(int number, int price, String date) {
+        this.number = number;
+        this.price = price;
+        this.date = date;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
