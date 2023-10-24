0ae2867b92671efe60a9a4d4eb36e368b63995b1
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-24 23:34:15 +0300

    commit

---
+++ pz_16/theatre/Actor.java
@@ -0,0 +1,45 @@
+public class Actor {
+    private String name;
+    private int age;
+    private String gender;
+    private int experience;
+
+    public Actor(String name, int age, String gender, int experience) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}
