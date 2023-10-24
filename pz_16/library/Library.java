f1f2c03b42e1cb451e78bfeb253a80b39e73d9ac
cherrra <94289157+cherrra@users.noreply.github.com>
2023-10-25 00:38:55 +0300

    commit

---
+++ pz_16/library/Library.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Library {
+    private String name;
+    private String address;
+    private ArrayList<Book> books;
+
+    public Library(String name, String address) {
+        this.name = name;
+        this.address = address;
+        books = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public ArrayList<Book> searchBookByTitle(String title) {
+        ArrayList<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+}
