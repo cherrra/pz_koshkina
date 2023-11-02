89e6ea951ee46ccde3e5617f42d57b70f67c9658
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-02 15:09:53 +0300

    commit

---
+++ pz_19/Main_button.java
@@ -0,0 +1,7 @@
+import javax.swing.JFrame;
+public class Main_button {
+    public static void main(String [] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new ContentPaneReplace();
+    }
+}
