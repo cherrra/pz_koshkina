ab8cda25d216b9470d73da178a98669fe7ef18fb
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-25 22:51:21 +0300

    commit

---
+++ pz_26/Main.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+public class Main {
+    public static void main(String args[]) {
+    blablabla blablabla = new blablabla();
+    blablabla.pack();
+    blablabla.setSize(new Dimension(800, 500));
+    blablabla.setVisible(true);
+    }
+}
