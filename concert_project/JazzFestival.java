3c99c0b38b9b557c7cff9d79208e4262b2a9b782
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-26 20:37:21 +0300

    commit

---
+++ concert_project/JazzFestival.java
@@ -0,0 +1,42 @@
+class JazzFestival extends Concert implements Festival {
+    private String[] musicians;
+    private String vibe;
+
+    public JazzFestival(String name, String date, String venue, double ticketPrice, String vibe) {
+        super(name, date, venue, ticketPrice);
+        this.musicians = musicians;
+        this.vibe = vibe;
+    }
+    @Override
+    public void getInfo() {
+        System.out.println("Джаз-фестиваль: " + name);
+        System.out.println("Дата: " + date);
+        System.out.println("Место: " + venue);
+        System.out.println("Музыканты: " + String.join(", ", musicians));
+        System.out.println("Настроение: " + vibe);
+        System.out.println("Стоимость: " + ticketPrice);
+    }
+
+    @Override
+    public void buyTicket() {
+        System.out.println("Билет на джаз-фестиваль купил: " + name);
+    }
+
+    @Override
+    public void getLineUp() {
+        System.out.println("Состав участников фестиваля: " + String.join(", ", musicians));
+    }
+
+    @Override
+    public void getAttractions() {
+
+    }
+
+    public void setVibe(String vibe) {
+        this.vibe = vibe;
+    }
+
+    public String getVibe() {
+        return ("Free Jazz");
+    }
+}
