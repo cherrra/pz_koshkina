2cb45b78ed6f94498a5848451f49d6bbca34570c
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-26 20:33:25 +0300

    commit

---
+++ concert_project/ClassicalConcert.java
@@ -0,0 +1,32 @@
+class ClassicalConcert extends Concert {
+    private String composer;
+    private String orchestra;
+
+    public ClassicalConcert(String name, String date, String venue, double ticketPrice, String composer) {
+        super(name, date, venue, ticketPrice);
+        this.composer = composer;
+        this.orchestra = orchestra;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Классический концерт: " + name);
+        System.out.println("Дата: " + date);
+        System.out.println("Место: " + venue);
+        System.out.println("Композитор: " + composer);
+        System.out.println("Оркестр: " + orchestra);
+        System.out.println("Стоимость: " + ticketPrice);
+    }
+
+    public void setComposer(String composer) {
+        this.composer = composer;
+    }
+
+    public String getComposer() {
+        return ("Шин Джихо");
+    }
+    @Override
+    public void buyTicket() {
+        System.out.println("Билет на классический концерт купил: " + name);
+    }
+}
