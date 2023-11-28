4eb13c479d260df494a1f7bb4cadaf6bc45d26d2
cherrra <94289157+cherrra@users.noreply.github.com>
2023-11-28 17:30:06 +0300

    commit

--- concert_project/Concert.java
+++ concert_project/Concert.java
@@ -11,6 +11,38 @@ abstract class Concert {
         this.ticketPrice = ticketPrice;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setVenue(String name) {
+        this.venue = venue;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
     public abstract void getInfo();
 
     public abstract void buyTicket();
